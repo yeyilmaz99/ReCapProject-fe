5b934a20fccaa4be01130c9f201d648244c5be54
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-10 21:10:23 +0300

    ResponseModel fix

--- src/app/services/rentalService/rental.service.ts
+++ src/app/services/rentalService/rental.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RentalResponseModel } from 'src/app/models/rentalModel/rentalResponseModel';
+import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { Rental } from 'src/app/models/rental';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,8 @@ export class RentalService {
   apiUrl = "https://localhost:44345/api/Rentals/getrentaldetails";
   constructor(private httpClient: HttpClient) { }
 
-  getRentals():Observable<RentalResponseModel>{
-    return this.httpClient.get<RentalResponseModel>(this.apiUrl);
+  getRentals():Observable<ListResponseModel<Rental>>{
+    return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl);
   }
 
 }
