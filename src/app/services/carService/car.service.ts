936cc037d4d4759afb4d40649ca31be9f3257555
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-11 20:56:49 +0300

    18.gün ödev son kısım

--- src/app/services/carService/car.service.ts
+++ src/app/services/carService/car.service.ts
@@ -3,18 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { Car } from 'src/app/models/car';
-
+import { ResponseModel } from 'src/app/models/responseModel';
+import { SignleResponseModel } from 'src/app/models/singleResponseModel';
+import { CarImage } from 'src/app/models/carImage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarService {
-  apiUrl = "https://localhost:44345/api/Cars/getcardetails";
+  apiUrl = 'https://localhost:44345/api/';
 
-  constructor(private httpClient: HttpClient ) { }
+  constructor(private httpClient: HttpClient) {}
 
+  getCars(): Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + 'Cars/getcarsdetails';
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
 
-  getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+  getCarsByBrand(brandId: number): Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + 'Cars/getbybrand?brandId=' + brandId;
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+  getCarsByColor(colorId: number): Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + 'Cars/getbycolor?colorId=' + colorId;
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+  getCarDetailsByCarId(carId: number): Observable<SignleResponseModel<Car>> {
+    let newPath = this.apiUrl + 'Cars/getcardetails?carId=' + carId;
+    return this.httpClient.get<SignleResponseModel<Car>>(newPath);
+  }
+  getCarImagesByCarId(carId: number): Observable<ListResponseModel<CarImage>> {
+    let newPath = this.apiUrl + 'CarImages/getall?carId=' + carId;
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath);
   }
 }
