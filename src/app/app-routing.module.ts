730bb4d08eb8578fb09457f8701273cfcbc1fafc
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-06-30 22:21:33 +0300

    Template added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandComponent } from './components/brand/brand.component';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 import { CarComponent } from './components/car/car.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: "", pathMatch:"full", component:CarComponent},
+  {path: "", pathMatch:"full", component:HomeComponent},
   {path:"cars", component: CarComponent},
   {path:"cars/brand/:brandId", component: CarComponent},
   {path:"cars/color/:colorId", component: CarComponent},
-  {path:"cars/car/:carId", component:CarDetailComponent}
+  {path:"cars/car/:carId", component:CarDetailComponent},
+  {path: "brand", component:BrandComponent}
 ];
 
 @NgModule({
