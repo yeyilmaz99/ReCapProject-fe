2b840ef42d2fce3271dade5dde54d80210887857
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-04 23:10:46 +0300

    19.gün ödev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { BrandComponent } from './components/brand/brand.component';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 import { CarComponent } from './components/car/car.component';
 import { HomeComponent } from './components/home/home.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 const routes: Routes = [
   {path: "", pathMatch:"full", component:HomeComponent},
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path:"cars/brand/:brandId", component: CarComponent},
   {path:"cars/color/:colorId", component: CarComponent},
   {path:"cars/car/:carId", component:CarDetailComponent},
-  {path: "brand", component:BrandComponent}
+  {path: "brand", component:BrandComponent},
+  {path: "rental", component:RentalComponent}
 ];
 
 @NgModule({
