1349575717f2575a0f7955d49da04a921af27259
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-04 23:10:58 +0300

    19.gün ödev

--- src/app/services/rentalService/rental.service.ts
+++ src/app/services/rentalService/rental.service.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { Rental } from 'src/app/models/rental';
+import { Rent } from 'src/app/models/rentModel';
+import { ResponseModel } from 'src/app/models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentalService {
-  apiUrl = "https://localhost:44345/api/Rentals/getrentaldetails";
+  apiUrl = "https://localhost:44345/api/Rentals/";
   constructor(private httpClient: HttpClient) { }
 
   getRentals():Observable<ListResponseModel<Rental>>{
-    return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl+"getrentaldetails");
+  }
+  addRental(rent:Rent):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"add",rent);
   }
 
 }
