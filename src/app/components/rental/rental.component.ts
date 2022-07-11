14a90caf0fe3b5969bd6226fb28eff5ccaccb81e
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-11 20:58:52 +0300

    19.gün ödev

--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -5,7 +5,12 @@ import { CarImage } from 'src/app/models/carImage';
 import { Rental } from 'src/app/models/rental';
 import { CarService } from 'src/app/services/carService/car.service';
 import { RentalService } from 'src/app/services/rentalService/rental.service';
-import {FormGroup, FormControl , FormBuilder ,Validators} from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { Rent } from 'src/app/models/rentModel';
 import { ToastrService } from 'ngx-toastr';
 
@@ -21,24 +26,25 @@ const year = today.getFullYear();
 export class RentalComponent implements OnInit {
   rentals: Rental[] = [];
   dataLoaded: boolean = false;
-  car:Car;
-  carId:number;
-  carImages:CarImage[] = [];
-  rent: Rent = { carId: 0, customerId: 0, rentDate: new Date(),returnDate:null };
+  car: Car;
+  carId: number;
+  carImages: CarImage[] = [];
+  rent: Rent = {
+    carId: 0,
+    customerId: 0,
+    rentDate: new Date(),
+    returnDate: null,
+  };
 
-
-
-
-
-  campaignOne:FormGroup
+  campaignOne: FormGroup;
 
   constructor(
-    private activatedRoute:ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     private rentalService: RentalService,
     private carService: CarService,
     private formBuilder: FormBuilder,
-    private toastrService:ToastrService
-    ) {}
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -70,36 +76,37 @@ export class RentalComponent implements OnInit {
     });
   }
 
-  rentaCar() {
-    this.rent.carId = this.carId;
-    this.rent.customerId = 1,
-    this.rent.rentDate = new Date();
-    this.rentalService.addRental(this.rent).subscribe(response => {
-      this.toastrService.success(response.message,"Başarılı");
-    },responseError => {
-      this.toastrService.error(responseError.error.message)
+  // rentaCar() {
+  //   this.rentalService.addRental(this.rent).subscribe(
+  //     (response) => {
+  //       this.toastrService.success(response.message, 'Başarılı');
+  //     },
+  //     (responseError) => {
+  //       this.toastrService.error(responseError.error.message);
+  //     }
+  //   );
+  // }
+
+  datePicker() {
+    this.campaignOne = this.formBuilder.group({
+      rentDate: [new Date(year, month, today.getDate()), Validators.required],
+      returnDate: [''],
+      customerId: [1, Validators.required],
+      carId: [this.carId, Validators.required],
     });
   }
 
-  datePicker(){
-    this.campaignOne = this.formBuilder.group({
-      rentDate:[new Date(year, month, today.getDate()), Validators.required],
-      returnDate:[""],
-      customerId:[1,Validators.required],
-      carId:[this.carId,Validators.required]
-
-    })
-
-  }
-
-  getDatePicker(){
-    if(this.campaignOne.valid){
-      
+  rentACar() {
+    if (this.campaignOne.valid) {
+      let rent: Rent = Object.assign({}, this.campaignOne.value);
+      this.rentalService.addRental(rent).subscribe(
+        (response) => {
+          this.toastrService.success(response.message, 'Başarılı');
+        },
+        (responseError) => {
+          this.toastrService.error(responseError.error.message);
+        }
+      );
     }
   }
-
-
-
-
-  
 }
