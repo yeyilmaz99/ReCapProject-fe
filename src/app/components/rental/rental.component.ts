75091b60b79facd735e29ba81310290c6b328106
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-11 20:51:24 +0300

    19.gün ödev

--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -7,6 +7,7 @@ import { CarService } from 'src/app/services/carService/car.service';
 import { RentalService } from 'src/app/services/rentalService/rental.service';
 import {FormGroup, FormControl , FormBuilder ,Validators} from '@angular/forms';
 import { Rent } from 'src/app/models/rentModel';
+import { ToastrService } from 'ngx-toastr';
 
 const today = new Date();
 const month = today.getMonth();
@@ -21,27 +22,28 @@ export class RentalComponent implements OnInit {
   rentals: Rental[] = [];
   dataLoaded: boolean = false;
   car:Car;
+  carId:number;
   carImages:CarImage[] = [];
+  rent: Rent = { carId: 0, customerId: 0, rentDate: new Date(),returnDate:null };
 
 
 
 
 
-  campaignOne = new FormGroup({
-    start: new FormControl(new Date(year, month, today.getDate())),
-    end: new FormControl(),
-  });
+  campaignOne:FormGroup
 
   constructor(
     private activatedRoute:ActivatedRoute,
     private rentalService: RentalService,
     private carService: CarService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private toastrService:ToastrService
     ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
       if (params['carId']) {
+        this.carId = params['carId'];
         this.getCarDetailsByCarId(params['carId']);
         this.getCarImagesByCarId(params['carId']);
       }
@@ -68,10 +70,23 @@ export class RentalComponent implements OnInit {
     });
   }
 
+  rentaCar() {
+    this.rent.carId = this.carId;
+    this.rent.customerId = 1,
+    this.rent.rentDate = new Date();
+    this.rentalService.addRental(this.rent).subscribe(response => {
+      this.toastrService.success(response.message,"Başarılı");
+    },responseError => {
+      this.toastrService.error(responseError.error.message)
+    });
+  }
+
   datePicker(){
     this.campaignOne = this.formBuilder.group({
-      start:[new Date, Validators.required],
-      end:[""],
+      rentDate:[new Date(year, month, today.getDate()), Validators.required],
+      returnDate:[""],
+      customerId:[1,Validators.required],
+      carId:[this.carId,Validators.required]
 
     })
 
@@ -79,7 +94,7 @@ export class RentalComponent implements OnInit {
 
   getDatePicker(){
     if(this.campaignOne.valid){
-      let rentModel:Rent
+      
     }
   }
 
