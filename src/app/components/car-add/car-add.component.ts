a077eed0cb2c0c2a53edfcfcf190c389382799f1
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-19 20:53:35 +0300

    20.gün add-car

--- src/app/components/car-add/car-add.component.ts
+++ src/app/components/car-add/car-add.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brandService/brand.service';
 
 @Component({
   selector: 'app-car-add',
   templateUrl: './car-add.component.html',
-  styleUrls: ['./car-add.component.css']
+  styleUrls: ['./car-add.component.css'],
 })
 export class CarAddComponent implements OnInit {
-
-  constructor() { }
+  brands: Brand[];
+  constructor(private brandService: BrandService) {}
 
   ngOnInit(): void {
+    this.getBrands();
   }
 
+  getBrands() {
+    this.brandService.getBrands().subscribe((response) => {
+      this.brands = response.data;
+      console.log(this.brands);
+    });
+  }
 }
