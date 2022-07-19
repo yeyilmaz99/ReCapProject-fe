d91b5930278af4b8190500389e5edacedc320b98
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-19 21:09:47 +0300

    20.gün add-car

--- src/app/components/car-add/car-add.component.ts
+++ src/app/components/car-add/car-add.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Brand } from 'src/app/models/brand';
 import { Color } from 'src/app/models/color';
 import { BrandService } from 'src/app/services/brandService/brand.service';
@@ -13,10 +18,12 @@ import { ColorService } from 'src/app/services/colorService/color.service';
 export class CarAddComponent implements OnInit {
   brands: Brand[];
   colors: Color[];
-  carAddForm:FormGroup;
-  constructor(private brandService: BrandService,
-    private colorService:ColorService,
-    private formBuilder:FormBuilder) {}
+  carAddForm: FormGroup;
+  constructor(
+    private brandService: BrandService,
+    private colorService: ColorService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.getBrands();
@@ -37,14 +44,20 @@ export class CarAddComponent implements OnInit {
     });
   }
 
-  createCarAddForm(){
+  createCarAddForm() {
     this.carAddForm = this.formBuilder.group({
-      brandId:["",Validators.required],
-      colorId:["",Validators.required],
-      carName:["",Validators.required],
-      modelYear:["",Validators.required],
-      dailyPrice:["",Validators.required],
-      description:["",Validators.required]
-    })
+      brandId: ['', Validators.required],
+      colorId: ['', Validators.required],
+      carName: ['', Validators.required],
+      modelYear: ['', Validators.required],
+      dailyPrice: ['', Validators.required],
+      description: ['', Validators.required],
+    });
+  }
+
+  addCar() {
+    if (this.carAddForm.valid) {
+      console.log(this.carAddForm.value);
+    }
   }
 }
