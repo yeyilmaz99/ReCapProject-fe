3687c18057a04658c2a526a3775cc72571b0c68d
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-20 20:03:56 +0300

    20.gün add-color

--- src/app/services/colorService/color.service.ts
+++ src/app/services/colorService/color.service.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Color } from 'src/app/models/color';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ResponseModel } from 'src/app/models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
-  apiUrl = "https://localhost:44345/api/Colors/getall";
+  apiUrl = "https://localhost:44345/api/Colors/";
   constructor(private httpClient: HttpClient) { }
 
 
   getColors():Observable<ListResponseModel<Color>>{
-    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl+"getall");
+  }
+
+  addCar(color:Color):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"add",color)
   }
 }
