b6cb4a5d17fb599d877f8916f60b5a085669ce50
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-20 19:48:13 +0300

    20.gün add-color

--- src/app/components/color-add/color-add.component.ts
+++ src/app/components/color-add/color-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorService } from 'src/app/services/colorService/color.service';
 
 @Component({
   selector: 'app-color-add',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private colorService:ColorService) { }
 
   ngOnInit(): void {
   }
