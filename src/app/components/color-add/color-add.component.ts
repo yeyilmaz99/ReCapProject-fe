15a78217df5ca6a7171cc470094e342f500c98da
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-22 17:50:43 +0300

    20.gün update

--- src/app/components/color-add/color-add.component.ts
+++ src/app/components/color-add/color-add.component.ts
@@ -43,6 +43,7 @@ export class ColorAddComponent implements OnInit {
       let colorToAdd:Color = Object.assign({},this.colorForm.value);
       this.colorService.addColor(colorToAdd).subscribe(response => {
         this.toastrService.success(response.message);
+        this.getColors();
       },responseError => {
         this.toastrService.error(responseError.error.message)
       })
@@ -60,7 +61,7 @@ export class ColorAddComponent implements OnInit {
       let colorToUpdate = Object.assign({},this.colorUpdateForm.value)
       this.colorService.updateColor(colorToUpdate).subscribe(response => {
         this.toastrService.success(response.message);
-        
+        this.getColors();
       },responseError => {
         this.toastrService.error(responseError.message);
       })
