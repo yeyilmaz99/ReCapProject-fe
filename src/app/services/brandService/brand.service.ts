8e88710f08a0d276e871013e608d59b3f29d7012
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-07-25 16:39:14 +0300

    20.gün deletion

--- src/app/services/brandService/brand.service.ts
+++ src/app/services/brandService/brand.service.ts
@@ -26,7 +26,7 @@ export class BrandService {
   }
 
   deleteBrand(brand:Brand):Observable<ResponseModel>{
-    return this.httpClient.delete<ResponseModel>(this.apiUrl+"delete");
+    return this.httpClient.delete<ResponseModel>(this.apiUrl+"delete" , {body:brand});
   }
 
 
