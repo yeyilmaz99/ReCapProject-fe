c4eba247c00cabe31014b166ce98ddc065a6699c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-08-24 20:40:23 +0300

    login-register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
+  
+
+  createRegisterForm(){
+    this.registerForm = this.formBuilder.group({
+      
+    })
+  }
+  
+
+
+
+
 
 }
