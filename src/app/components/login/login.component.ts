6e42cf1c9b09ed3f0e833db55e2c37e673f0d459
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-08-24 21:55:47 +0300

    login-register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { LoginModel } from 'src/app/models/login';
+import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm:FormGroup
+  constructor(
+    private formBuilder:FormBuilder,
+    private authService:AuthService,
+    private toastrService:ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+
+  createLoginForm(){
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      let loginModel: LoginModel = Object.assign({},this.loginForm.value)
+      this.authService.login(loginModel).subscribe((response)=>{
+        console.log(response);
+        localStorage.setItem('token', response.data.token);
+      })
+    }
   }
 
 }
