8ccbe9a72257e9b5138cfd87829b874328345a6d
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-05 18:09:11 +0300

    Jwt-decoder

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -5,15 +5,15 @@ import { LoginModel } from 'src/app/models/login';
 import { RegisterModel } from 'src/app/models/register';
 import { SingleResponseModel } from 'src/app/models/singleResponseModel';
 import { TokenModel } from 'src/app/models/tokenModel';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Claims } from 'src/app/models/claims';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   apiUrl = 'https://localhost:44345/api/Auth/'
-  constructor(private httpClient:HttpClient) { }
-
-
+  constructor(private httpClient:HttpClient, private jwtHelper: JwtHelperService) { }
 
   login(loginModel:LoginModel):Observable<SingleResponseModel<TokenModel>>{
     let newPath = this.apiUrl + "login";
@@ -28,11 +28,39 @@ export class AuthService {
 
   isAuthenticated(){
     if(localStorage.getItem("token")){
+      this.getClaims()
       return true;
     }
     else{
       return false;
     }
   }
+  getClaims(){
+    let token = this.getToken();
+    if(token != null){
+      let tokenDetails = Object.entries(this.jwtHelper.decodeToken(token))
+      console.log(tokenDetails);
+      let claims : Claims = new Claims;
+      tokenDetails.forEach(detail => {
+        switch(detail[0]){
+          case "email": {
+            claims.email = String(detail[1]);
+            break;
+          }
+          case "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier":{
+            claims.userId = Number(detail[1])
+          }
+        }
+      })
+        
+      
+    }
+  }
+
+  getToken(){
+    return localStorage.getItem("token");
+  }
+
+
 
 }
