47958f98690784e2e1e61982707f4bf2f0153b17
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-06 19:12:20 +0300

    userProfileSettings

---
+++ src/app/components/user-profile-settings/user-profile-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-settings',
+  templateUrl: './user-profile-settings.component.html',
+  styleUrls: ['./user-profile-settings.component.css']
+})
+export class UserProfileSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
