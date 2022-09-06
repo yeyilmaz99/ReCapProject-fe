27f736bd8465b64e33ab122dd8d7b37114676013
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-06 18:14:26 +0300

    navbar

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Claims } from 'src/app/models/claims';
+import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-navi',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.getClaims();
   }
+  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
+
+
+
+  isLoggedIn(): boolean{
+    if(this.authService.isAuthenticated() == true){
+      return true;
+
+    }else{
+      return false;
+    }
+  }
+
+  getClaims(){
+    if(this.isLoggedIn()){
+      let claims:Claims | undefined = this.authService.getClaims();
+      this.claims = claims;
+    }
+  }
+
 
 }
