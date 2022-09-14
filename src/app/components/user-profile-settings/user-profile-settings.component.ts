678f456d693624c832be0f92eb1c28f9ffd0580e
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-14 23:52:15 +0300

    profileSettings

--- src/app/components/user-profile-settings/user-profile-settings.component.ts
+++ src/app/components/user-profile-settings/user-profile-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Claims } from 'src/app/models/claims';
+import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-user-profile-settings',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.getClaims();
   }
 
+  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
+  firstName:string|undefined = "";
+  lastName:string|undefined = "";
+
+
+  getClaims(){
+    if(this.authService.isAuthenticated()){
+      let claims:Claims | undefined = this.authService.getClaims();
+      this.claims = claims;
+      let mySplitResult = this.claims?.fullName.split(" ");
+      this.lastName =  mySplitResult[mySplitResult.length-1];
+      const lastIndexOfSpace = claims.fullName.lastIndexOf(' ');
+      this.firstName = claims.fullName.substring(0, lastIndexOfSpace)
+    }
+  }
 }
