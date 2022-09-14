df96223684d76f9b1f90989d0eb0b99ef66bbdec
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-14 23:19:43 +0300

    localStorageService

--- src/app/services/localStorage/local-storage.service.ts
+++ src/app/services/localStorage/local-storage.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
+
+
+
+  getItem(name:string){
+    return localStorage.getItem("token");
+  }
+
+  setItem(name:string, data:string){
+    localStorage.setItem(name,data);
+  }
+
+  deleteItem(name:string){
+    localStorage.removeItem(name);
+  }
+
+
 }
