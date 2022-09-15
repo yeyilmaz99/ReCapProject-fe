40df7a04aa9833873703b3c148a53cdfe13f35e8
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-15 14:53:26 +0300

    userSettings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'RecapProject-fe';
+export class AppComponent  {
 }
