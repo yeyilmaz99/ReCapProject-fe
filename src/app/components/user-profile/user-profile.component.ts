841dcd7e8f25949d0e44666cb0640fb1c778d10c
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-15 14:53:36 +0300

    userSettings

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -11,15 +11,48 @@ export class UserProfileComponent implements OnInit {
 
 
   constructor(private authService:AuthService) { }
+  userInfo = true;
+  userSettings = false;
+  userRentals = false;
+  userCompany = false;
+  enableInfo(){
+    this.userInfo =true;
+    this.userSettings = false;
+    this.userRentals = false;
+    this.userCompany = false;
+
+  }
+  enableSettings(){
+    this.userInfo =false;
+    this.userSettings = true;
+    this.userRentals = false;
+    this.userCompany = false;
+
+  }
+  enableRentals(){
+    this.userInfo =false;
+    this.userSettings = false;
+    this.userRentals = true;
+    this.userCompany = false;
+
+  }
+  enableCompany(){
+    this.userInfo =false;
+    this.userSettings = false;
+    this.userRentals = false;
+    this.userCompany = true;
+
+  }
 
   ngOnInit(): void {
     this.getClaims();
   }
 
-  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
-  firstName:string|undefined = "";
-  lastName:string|undefined = "";
+  activeMenu(event:any){
+    event.target.classList.add("className")
+  }
 
+  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
 
   getClaims(){
     if(this.authService.isAuthenticated()){
