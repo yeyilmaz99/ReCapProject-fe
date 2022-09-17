c9ac9d0ed2ce52f0be94cb0a26fa1bfd39bb2652
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-18 00:47:15 +0300

    userRentals

--- src/app/services/rentalService/rental.service.ts
+++ src/app/services/rentalService/rental.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { Rental } from 'src/app/models/rental';
+import { Rentals } from 'src/app/models/rentals';
 import { Rent } from 'src/app/models/rentModel';
 import { ResponseModel } from 'src/app/models/responseModel';
 
@@ -21,6 +22,9 @@ export class RentalService {
   }
   checkIfCarIsReturned(id:number):Observable<ResponseModel>{
     return this.httpClient.get<ResponseModel>(this.apiUrl+"checkifcarisreturned?id="+id);
+  }
+  getAllRentals():Observable<ListResponseModel<Rental>>{
+    return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl+"getall");
   }
 
 }
