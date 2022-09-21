46ca718becf55aa7827abb37016ee351e73af5ca
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-21 20:40:47 +0300

    userFavorites

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Favorite } from 'src/app/models/favorite';
+import { FavoriteService } from 'src/app/services/favoriteService/favorite.service';
 
 @Component({
   selector: 'app-favorites',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private favoriteService:FavoriteService) { }
+  favorites:Favorite[] = []
 
   ngOnInit(): void {
+    this.getFavoritesByUserId();
   }
+
+
+  getFavoritesByUserId(userId:number = 1002){
+    this.favoriteService.getFavorites(userId).subscribe(response => {
+      this.favorites = response.data;
+    })
+  }
+
 
 }
