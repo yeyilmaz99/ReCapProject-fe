f89090f4241230d22f5a0bf3dec75f319d18a8eb
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-22 23:08:12 +0300

    userFavorites

--- src/app/services/favoriteService/favorite.service.ts
+++ src/app/services/favoriteService/favorite.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Favorite } from 'src/app/models/favorite';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ResponseModel } from 'src/app/models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,11 @@ export class FavoriteService {
     let newPath = this.apiUrl + "getfavoritesbyuserid?id="
     return this.httpClient.get<ListResponseModel<Favorite>>(newPath+userId);
   }
+
+  addToFavorites(favorite:Favorite):Observable<ResponseModel>{
+    let newPath = this.apiUrl + "add";
+    return this.httpClient.post<ResponseModel>(newPath,favorite);
+  }
+
+
 }
