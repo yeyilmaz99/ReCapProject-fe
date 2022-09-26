b3d4560df02de22dbc940cbaafc866ece6f41401
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-09-26 23:48:53 +0300

    userRentals

--- src/app/services/rentalService/rental.service.ts
+++ src/app/services/rentalService/rental.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { Rental } from 'src/app/models/rental';
+import { RentalDetails } from 'src/app/models/rentalDetails';
 import { Rentals } from 'src/app/models/rentals';
 import { Rent } from 'src/app/models/rentModel';
 import { ResponseModel } from 'src/app/models/responseModel';
@@ -27,9 +28,8 @@ export class RentalService {
     return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl+"getall");
   }
 
-  getRentalsByUserId(userId:number):Observable<ListResponseModel<Rental>>{
-    
-    return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl+"getrentaldetailsbyuserid?userId="+ userId);
+  getRentalsByUserId(userId:number):Observable<ListResponseModel<RentalDetails>>{
+    return this.httpClient.get<ListResponseModel<RentalDetails>>(this.apiUrl+"getrentaldetailsbyuserid?userId="+ userId);
   }
 
 }
