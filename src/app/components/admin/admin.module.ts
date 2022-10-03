9c25a63ee95f19ca391aab81817546c75acbb8be
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-04 02:34:34 +0300

    update

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -3,8 +3,8 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminRoutes } from './admin.routing';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';   
-import { AdminCarListComponent } from './components/admin-car-list/admin-car-list.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';   
+import { AdminCarListComponent } from './admin-car-list/admin-car-list.component'; 
 
 
 
