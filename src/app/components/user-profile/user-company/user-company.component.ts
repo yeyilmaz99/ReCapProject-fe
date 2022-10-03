8251f997ca46ee3697fc338290beeaf0a742b2ba
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-04 02:38:24 +0300

    update

---
+++ src/app/components/user-profile/user-company/user-company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-company',
+  templateUrl: './user-company.component.html',
+  styleUrls: ['./user-company.component.css']
+})
+export class UserCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
