cbee2567481a6f8f6a6d9f5a14d944f7201155c8
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-04 02:38:04 +0300

    update

---
+++ src/app/components/user-profile/user.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { UserRoutes } from './user.routing';
+import { UserRentalsComponent } from './user-rentals/user-rentals.component';
+import { UserCompanyComponent } from './user-company/user-company.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FavoritesComponent } from '../favorites/favorites.component';
+
+
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(UserRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatTabsModule
+
+  ],
+  declarations: [
+    UserRentalsComponent,
+    UserCompanyComponent,
+    UserSettingsComponent,
+    UserInfoComponent,
+    FavoritesComponent
+  ]
+})
+
+export class UserModule {}
