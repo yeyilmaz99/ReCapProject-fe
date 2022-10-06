cc7a338f8df68189572d11587bc38fc38a702016
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-06 21:09:00 +0300

    addingComponents

--- src/app/components/admin/admin.routing.ts
+++ src/app/components/admin/admin.routing.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 
 import { AdminCarListComponent } from './admin-car-list/admin-car-list.component'; 
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminRentalListComponent } from './admin-rental-list/admin-rental-list.component';
+import { AdminUserListComponent } from './admin-user-list/admin-user-list.component';
 
 
 export const AdminRoutes:Routes = [
     {path: 'dashboard', component:AdminDashboardComponent},
-    {path: 'car-list', component:AdminCarListComponent}
+    {path: 'car-list', component:AdminCarListComponent},
+    {path: 'user-list', component:AdminUserListComponent},
+    {path: 'rental-list', component:AdminRentalListComponent}
 ]
