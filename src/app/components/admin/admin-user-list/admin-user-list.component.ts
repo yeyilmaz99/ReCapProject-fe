38b76eafe244a787e27766cfab0a17d5d920c147
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-07 22:15:19 +0300

    admin userlist

--- src/app/components/admin/admin-user-list/admin-user-list.component.ts
+++ src/app/components/admin/admin-user-list/admin-user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/userService/user.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';  
 
 @Component({
   selector: 'app-admin-user-list',
@@ -27,4 +28,29 @@ export class AdminUserListComponent implements OnInit {
       this.dataLoaded = true;
     })
   }
+
+  delete(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
+    
+  }
+
+
+
+
 }
