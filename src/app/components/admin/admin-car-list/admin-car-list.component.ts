9332225f1105c4f8f42f057f86d696aa41cf2c45
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-07 21:50:41 +0300

    admin

--- src/app/components/admin/admin-car-list/admin-car-list.component.ts
+++ src/app/components/admin/admin-car-list/admin-car-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Car } from 'src/app/models/car';
+import { CarDelete } from 'src/app/models/carDelete';
 import { CarService } from 'src/app/services/carService/car.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';  
 
 @Component({
   selector: 'app-admin-car-list',
@@ -11,7 +14,8 @@ export class AdminCarListComponent implements OnInit {
   cars:Car[] = [];
   dataLoaded:boolean = false;
   constructor(
-    private carService:CarService
+    private carService:CarService,
+    private toastrService:ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -23,6 +27,33 @@ export class AdminCarListComponent implements OnInit {
       this.cars = response.data;
       this.dataLoaded = true;
     })
+  }
+
+
+  delete(carToDeleteId:number){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+        let carToDelete: CarDelete = {id:carToDeleteId,brandId:0,carName:'',colorId:0,dailyPrice:0,description:'',modelYear:0}
+        this.carService.deleteCar(carToDelete).subscribe(response => {
+          this.toastrService.warning(response.message)
+        })
+        this.getCars();
+      }
+    })
+    
   }
 
 }
