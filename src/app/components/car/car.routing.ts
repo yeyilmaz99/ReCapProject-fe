eda1a0298c7f7f3ab3c32389f596db9fdd295387
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-11 18:03:51 +0300

    car routing

---
+++ src/app/components/car/car.routing.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { LoginGuard } from 'src/app/guards/login.guard';
+import { CarAddComponent } from './car-add/car-add.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
+import { CarsComponent } from './cars/cars.component';
+
+
+
+
+export const CarRoutes:Routes = [
+    {path:  "car/:carId", component:CarDetailComponent},
+    {path:  "add", component:CarAddComponent, canActivate:[LoginGuard]},
+    {path: "", pathMatch:"full", component:CarsComponent}
+]
