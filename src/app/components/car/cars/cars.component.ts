ce6a92fe502882464b45d4fc6802504696929e9e
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-11 18:04:37 +0300

    cars component

---
+++ src/app/components/car/cars/cars.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute } from '@angular/router';
+import { Brand } from 'src/app/models/brand';
+import { Car } from 'src/app/models/car';
+import { Color } from 'src/app/models/color';
+import { FilterModel } from 'src/app/models/filterModel';
+import { BrandService } from 'src/app/services/brandService/brand.service';
+import { CarService } from 'src/app/services/carService/car.service';
+import { ColorService } from 'src/app/services/colorService/color.service';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+  cars: Car[] = [];
+  length: number = this.cars.length;
+  carsSlice: Car[];
+  pageSize = 10;
+  brands: Brand[];
+  colors: Color[];
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  dataLoaded: boolean = false;
+  carFilterForm: FormGroup;
+
+  constructor(
+    private carService: CarService,
+    private activatedRoute: ActivatedRoute,
+    private colorService: ColorService,
+    private brandService: BrandService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+
+    this.getCars();
+    this.getColors();
+    this.getBrands();
+    this.createCarFilterForm();
+  }
+
+  getCars() {
+    this.carService.getCars().subscribe((response) => {
+      this.cars = response.data;
+      this.dataLoaded = true;
+    });
+  }
+  getBrands() {
+    this.brandService.getBrands().subscribe((response) => {
+      this.brands = response.data;
+      this.dataLoaded = true;
+    });
+  }
+  getColors() {
+    this.colorService.getColors().subscribe((response) => {
+      this.colors = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getCarsByBrand(brandId: number) {
+    this.carService.getCarsByBrand(brandId).subscribe((response) => {
+      this.cars = response.data;
+      this.dataLoaded = true;
+    });
+  }
+  getCarsByColor(colorId: number) {
+    this.carService.getCarsByColor(colorId).subscribe((response) => {
+      this.cars = response.data;
+      this.dataLoaded = true;
+    });
+  }
+  OnPageChange(event: PageEvent) {
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.cars.length) {
+      endIndex = this.cars.length;
+    }
+    this.carsSlice = this.cars.slice(startIndex, endIndex);
+  }
+
+  createCarFilterForm() {
+    this.carFilterForm = this.formBuilder.group({
+      colorId: ['', Validators.required],
+      brandId: ['', Validators.required],
+    });
+  }
+  filter() {
+    if (this.carFilterForm.valid) {
+      let filterModel: FilterModel = Object.assign(
+        {},
+        this.carFilterForm.value
+      );
+      this.carService
+        .getCarsByBrandAndColorId(filterModel.colorId, filterModel.brandId)
+        .subscribe((response) => {
+          this.cars = response.data;
+        });
+    } else {
+    }
+  }
+
+
+
+}
