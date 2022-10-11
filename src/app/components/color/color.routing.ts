7a231037011c45a272989f7984968e40943b10a4
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-11 18:05:46 +0300

    color routing

---
+++ src/app/components/color/color.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ColorAddComponent } from './color-add/color-add.component';
+
+
+
+
+export const ColorRoutes:Routes = [
+    {path:"settings", component:ColorAddComponent}
+]
