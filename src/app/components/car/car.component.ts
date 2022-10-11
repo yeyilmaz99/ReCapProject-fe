65cab17c1ca413fc89029f037893897fe3c6b481
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-11 18:03:41 +0300

    car component

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -22,98 +22,11 @@ import { AuthService } from 'src/app/services/authService/auth.service';
   styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit {
-  cars: Car[] = [];
-  length: number = this.cars.length;
-  carsSlice: Car[];
-  pageSize = 10;
-  brands: Brand[];
-  colors: Color[];
-  pageSizeOptions: number[] = [5, 10, 25, 100];
-  dataLoaded: boolean = false;
-  carFilterForm: FormGroup;
 
   constructor(
-    private carService: CarService,
-    private activatedRoute: ActivatedRoute,
-    private colorService: ColorService,
-    private brandService: BrandService,
-    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
-      if (params['brandId']) {
-        this.getCarsByBrand(params['brandId']);
-      } else if (params['colorId']) {
-        this.getCarsByColor(params['colorId']);
-      } else {
-        this.getCars();
-      }
-    });
-    this.getColors();
-    this.getBrands();
-    this.createCarFilterForm();
-  }
-
-  getCars() {
-    this.carService.getCars().subscribe((response) => {
-      this.cars = response.data;
-      this.dataLoaded = true;
-    });
-  }
-  getBrands() {
-    this.brandService.getBrands().subscribe((response) => {
-      this.brands = response.data;
-      this.dataLoaded = true;
-    });
-  }
-  getColors() {
-    this.colorService.getColors().subscribe((response) => {
-      this.colors = response.data;
-      this.dataLoaded = true;
-    });
-  }
-
-  getCarsByBrand(categoryId: number) {
-    this.carService.getCarsByBrand(categoryId).subscribe((response) => {
-      this.cars = response.data;
-      this.dataLoaded = true;
-    });
-  }
-  getCarsByColor(colorId: number) {
-    this.carService.getCarsByColor(colorId).subscribe((response) => {
-      this.cars = response.data;
-      this.dataLoaded = true;
-    });
-  }
-  OnPageChange(event: PageEvent) {
-    const startIndex = event.pageIndex * event.pageSize;
-    let endIndex = startIndex + event.pageSize;
-    if (endIndex > this.cars.length) {
-      endIndex = this.cars.length;
-    }
-    this.carsSlice = this.cars.slice(startIndex, endIndex);
-  }
-
-  createCarFilterForm() {
-    this.carFilterForm = this.formBuilder.group({
-      colorId: ['', Validators.required],
-      brandId: ['', Validators.required],
-    });
-  }
-  filter() {
-    if (this.carFilterForm.valid) {
-      let filterModel: FilterModel = Object.assign(
-        {},
-        this.carFilterForm.value
-      );
-      this.carService
-        .getCarsByBrandAndColorId(filterModel.colorId, filterModel.brandId)
-        .subscribe((response) => {
-          this.cars = response.data;
-        });
-    } else {
-    }
   }
 
 
