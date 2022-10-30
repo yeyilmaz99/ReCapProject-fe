27f48fa9dacdb396cde4cfbff2dd0ad6273a6825
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-10-31 02:31:06 +0300

    admin

--- src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from 'src/app/models/brand';
 import { Car } from 'src/app/models/car';
 import { Rental } from 'src/app/models/rental';
 import { User } from 'src/app/models/user';
+import { BrandService } from 'src/app/services/brandService/brand.service';
 import { CarService } from 'src/app/services/carService/car.service';
 import { RentalService } from 'src/app/services/rentalService/rental.service';
 import { UserService } from 'src/app/services/userService/user.service';
@@ -15,19 +17,23 @@ export class AdminDashboardComponent implements OnInit {
   cars:Car[] = [];
   rentals:Rental[] = [];
   users:User[] = [];
+  brands:Brand[] = [];
   usersDataLoaded:boolean = false;
   rentalsDataLoaded:boolean = false;
   carsDataLoaded:boolean = false;
+  brandsDataLoaded:boolean = false;
   constructor(
     private rentalService:RentalService,
     private carService:CarService,
-    private userService:UserService
+    private userService:UserService,
+    private brandService:BrandService
   ) { }
 
   ngOnInit(): void {
     this.getCars();
     this.getRentals();
     this.getUsers();
+    this.getBrands();
   }
 
   getCars(){
@@ -48,6 +54,13 @@ export class AdminDashboardComponent implements OnInit {
     this.userService.getUsers().subscribe(response => {
       this.users = response.data;
       this.usersDataLoaded = true;
+    })
+  }
+
+  getBrands(){
+    this.brandService.getBrands().subscribe(response =>{
+      this.brands = response.data;
+      this.brandsDataLoaded = true;
     })
   }
 
