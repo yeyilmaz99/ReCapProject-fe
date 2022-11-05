68f7e517de63bd9974b04910114c265601a55677
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-05 03:44:48 +0300

    user-favorites

---
+++ src/app/components/user-profile/user-favorites/user-favorites.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Claims } from 'src/app/models/claims';
+import { Favorite } from 'src/app/models/favorite';
+import { AuthService } from 'src/app/services/authService/auth.service';
+import { FavoriteService } from 'src/app/services/favoriteService/favorite.service';
+
+@Component({
+  selector: 'app-user-favorites',
+  templateUrl: './user-favorites.component.html',
+  styleUrls: ['./user-favorites.component.css']
+})
+export class UserFavoritesComponent implements OnInit {
+  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
+  dataLoaded:boolean = false;
+
+
+  constructor(
+    private favoriteService:FavoriteService,
+    private toastrService:ToastrService,
+    private authService:AuthService
+    ) { }
+  favorites:Favorite[] = []
+
+  ngOnInit(): void {
+    this.getClaims();
+    this.getRecentFavoritesByUserId();
+  }
+
+  getRecentFavoritesByUserId(){
+    this.favoriteService.getRecentFavoritesByUserId(this.claims.userId).subscribe(response =>{
+      this.favorites = response.data;
+      this.dataLoaded = true;
+      this.checkFavorites();
+    })
+  }
+
+  getClaims(){
+    if(this.authService.isAuthenticated()){
+      let claims:Claims | undefined = this.authService.getClaims();
+      this.claims = claims;
+    }
+  }
+
+  deleteFromFavorites(carId:number){
+    let favoriteToDelete:Favorite = {brandName:"",carName:"",carId:carId,colorName:"",dailyPrice:0,description:"",userId:this.claims.userId,userName:""}
+    this.favoriteService.deleteFromFavorites(favoriteToDelete).subscribe(response=>{
+      this.toastrService.error(response.message,"Deleted From Favorites")
+    },responseError=>{
+      this.toastrService.error(responseError.error.message);
+    })
+    this.getRecentFavoritesByUserId();
+  }
+
+
+  checkFavorites(){
+    if(this.favorites.length < 1 ){
+      let element = document.querySelector(".favorites");
+      element.innerHTML = "You dont have any favorites yet"
+    }
+  }}
