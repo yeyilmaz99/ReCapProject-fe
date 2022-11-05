b14ff516d89f6dd2dec4e8f08a12093dc0d62c03
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-05 03:41:41 +0300

    update

--- src/app/components/car/car.routing.ts
+++ src/app/components/car/car.routing.ts
@@ -3,12 +3,14 @@ import { LoginGuard } from 'src/app/guards/login.guard';
 import { CarAddComponent } from './car-add/car-add.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
 import { CarsComponent } from './cars/cars.component';
+import { RentalComponent } from './rental/rental.component';
 
 
 
 
 export const CarRoutes:Routes = [
     {path:  "car/:carId", component:CarDetailComponent},
-    {path:  "add", component:CarAddComponent},
-    {path: "", pathMatch:"full", component:CarsComponent}
+    {path:  "settings", component:CarAddComponent},
+    {path: "", pathMatch:"full", component:CarsComponent},
+    {path:  "car/:carId/rental", component:RentalComponent, canActivate:[LoginGuard]},
 ]
