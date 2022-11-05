2d58a096ce0f71f403799bfa5c413fabb4f9ff50
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-05 03:44:38 +0300

    update

--- src/app/components/user-profile/user.routing.ts
+++ src/app/components/user-profile/user.routing.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 
 import { UserCompanyComponent } from './user-company/user-company.component';
 import { UserInfoComponent } from './user-info/user-info.component';
-import { UserRentalsComponent } from './user-rentals/user-rentals.component';
 import { UserSettingsComponent } from './user-settings/user-settings.component';    
 
 
 export const UserRoutes:Routes = [
     {path: 'companysettings', component:UserCompanyComponent},
     {path: 'info', component:UserInfoComponent},
-    {path: 'rentals', component:UserRentalsComponent},
     {path: 'settings', component:UserSettingsComponent},
 ]
