4fe6be5cba8a0eb0b4255a22e8e9cd788224f5a9
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-08 04:19:02 +0300

    update

--- src/app/components/user-profile/components/user-sidebar/user-sidebar.component.ts
+++ src/app/components/user-profile/components/user-sidebar/user-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Claims } from 'src/app/models/claims';
+import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-user-sidebar',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.getClaims();
+  }
+
+  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
+
+  getClaims(){
+    if(this.authService.isAuthenticated()){
+      let claims:Claims | undefined = this.authService.getClaims();
+      this.claims = claims;
+    }
   }
 
 }
