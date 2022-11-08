4336d253b7d895e2e7bec3258887fa2d54388fb5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-08 04:18:50 +0300

    update

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -10,56 +10,12 @@ import { AuthService } from 'src/app/services/authService/auth.service';
 export class UserProfileComponent implements OnInit {
 
 
-  constructor(private authService:AuthService) { }
-  userInfo = true;
-  userSettings = false;
-  userRentals = false;
-  userCompany = false;
-  enableInfo(){
-    this.userInfo =true;
-    this.userSettings = false;
-    this.userRentals = false;
-    this.userCompany = false;
-
-  }
-  enableSettings(){
-    this.userInfo =false;
-    this.userSettings = true;
-    this.userRentals = false;
-    this.userCompany = false;
-
-  }
-  enableRentals(){
-    this.userInfo =false;
-    this.userSettings = false;
-    this.userRentals = true;
-    this.userCompany = false;
-
-  }
-  enableCompany(){
-    this.userInfo =false;
-    this.userSettings = false;
-    this.userRentals = false;
-    this.userCompany = true;
-
-  }
-
   ngOnInit(): void {
-    this.getClaims();
+
   }
 
-  activeMenu(event:any){
-    event.target.classList.add("className")
-  }
 
-  claims:Claims | undefined = {email:"",fullName:"",roles:[""],userId:0};
 
-  getClaims(){
-    if(this.authService.isAuthenticated()){
-      let claims:Claims | undefined = this.authService.getClaims();
-      this.claims = claims;
-    }
-  }
 
 
 }
