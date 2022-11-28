ab20e12e581cd104ca6b51eec797ec217c40465e
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-11-28 04:10:04 +0300

    findeks

---
+++ src/app/services/findeksService/findeks.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Findeks } from 'src/app/models/findeks';
+import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { SingleResponseModel } from 'src/app/models/singleResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FindeksService {
+
+  constructor(private httpClient:HttpClient) { }
+  apiUrl = "https://localhost:5001/api/Findeks/";
+
+
+  getAll():Observable<ListResponseModel<Findeks>>{
+    let newPath = this.apiUrl + "getall";
+    return this.httpClient.get<ListResponseModel<Findeks>>(newPath);
+  }
+
+  getByUserId(userId:number):Observable<SingleResponseModel<Findeks>>{
+    let newPath = this.apiUrl + "getByUserId?userId=";
+    return this.httpClient.get<SingleResponseModel<Findeks>>(newPath + userId)
+  }
+
+}
