554545ac3fef4d6d1875568f464535b0e6a3d519
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-04 02:50:32 +0300

    carFindeksPoint

--- src/app/services/findeksService/findeks.service.ts
+++ src/app/services/findeksService/findeks.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CarFindeks } from 'src/app/models/carFindeks';
 import { Findeks } from 'src/app/models/findeks';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { SingleResponseModel } from 'src/app/models/singleResponseModel';
@@ -11,17 +12,29 @@ import { SingleResponseModel } from 'src/app/models/singleResponseModel';
 export class FindeksService {
 
   constructor(private httpClient:HttpClient) { }
-  apiUrl = "https://localhost:5001/api/Findeks/";
+  apiUrl = "https://localhost:5001/api/";
 
 
   getAll():Observable<ListResponseModel<Findeks>>{
-    let newPath = this.apiUrl + "getall";
+    let newPath = this.apiUrl + "Findeks/getall";
     return this.httpClient.get<ListResponseModel<Findeks>>(newPath);
   }
 
   getByUserId(userId:number):Observable<SingleResponseModel<Findeks>>{
-    let newPath = this.apiUrl + "getByUserId?userId=";
+    let newPath = this.apiUrl + "Findeks/getByUserId?userId=";
     return this.httpClient.get<SingleResponseModel<Findeks>>(newPath + userId);
   }
+
+  getAllCarFindeks():Observable<ListResponseModel<CarFindeks>>{
+    let newPath = this.apiUrl + "CarFindeks/getall";
+    return this.httpClient.get<ListResponseModel<CarFindeks>>(newPath);
+  }
+
+  getCarFindeksById(carId:number){
+    let newPath = this.apiUrl + "CarFindeks/getbyid?carId=";
+    return this.httpClient.get<SingleResponseModel<CarFindeks>>(newPath + carId);
+  }
+
+
 
 }
