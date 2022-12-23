c75c0a4bb60ad8dc2e4ad13cd88903dfc57c1200
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-12-23 08:42:03 +0300

    removed unnecessary lines

--- src/app/components/car/rentacar/rental.component.ts
+++ src/app/components/car/rentacar/rental.component.ts
@@ -174,9 +174,9 @@ export class RentalComponent implements OnInit {
     this.getUsersFindeksPoint();
     findeks.findeksPoint = this.userFindeksPoint + 100;
     this.findeksService.updateFindeks(findeks).subscribe(response =>{
-      console.log("başarıylan apdeyt edildi");
+
     },responseError =>{
-      console.log("Apdeyt edilirken batladı");
+
     })
   }
 
@@ -184,9 +184,9 @@ export class RentalComponent implements OnInit {
     let findeks:Findeks = {userId:this.claims.userId,id:0,findeksPoint:0};
     findeks.findeksPoint = 100;
     this.findeksService.addFindeks(findeks).subscribe(response => {
-      console.log("başarıylan yeni findeks eklendi");
+
     },responseError => {
-      console.log("yeni findeks eklenirken hata oluştu");
+
     })
   }
 
